577096da861411111665257fc1db343ca18fe653
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-03 15:08:07 -0500

    POST /api/post

--- index.js
+++ index.js
@@ -10,7 +10,24 @@ server.get('/', (req, res) => {
 
 // POST  /api/posts 
 server.post('/api/posts', (req, res) => {
-    
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res
+        .status(400)
+        .json({errorMessage: "Please provide title and contents for the post."})
+    } else {
+        db.insert(req.body)
+        .then(item => {
+            res
+            .status(201)
+            .json(item)
+        })
+        .catch(error => {
+            res
+            .status(500)
+            .json({errorMessage: 'There was an error while saving the user to the database'})
+        })
+    }
 })
 
 // POST   /api/posts/:id/comments
